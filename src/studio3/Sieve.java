6ac1001daa70b20a32c9141303730b793c52a485
davidwang <davidwang@10.232.132.69>
2024-02-08 12:48:25 -0600

    studio 3

--- src/studio3/Sieve.java
+++ src/studio3/Sieve.java
@@ -4,19 +4,25 @@ import java.util.Scanner;
 public class Sieve {
 
 	public static void main(String[] args) {
-		//boolean array
-		//boolean[] primeOrNot = new boolean[itemSieve];
-		//for (int i=0;i<itemSieve;i++)
-		//{
-		//	primeOrNot[i]= true;
-		//}
-		// filling sieve w numbers
 		Scanner itemSieveIn = new Scanner(System.in);
 		System.out.println("What is your n?");
 		int itemSieve = itemSieveIn.nextInt();
 		int itemSieveColumn = 10;
 		int itemSieveRow = itemSieve/10;
 		int[][] sieve = new int[itemSieveRow][itemSieveColumn];
+		//boolean array
+		boolean[][] primeOrNot = new boolean[itemSieveRow][itemSieveColumn];
+		for (int i=0;i<itemSieve;i++)
+			for (int x=0; x<itemSieveRow; x++)
+			{	
+				for (int y=0;y<itemSieveColumn;y++)
+				{
+					primeOrNot[x][y]=true;
+
+				}
+			}
+		// filling sieve w numbers
+		
 		for (int x=0; x<itemSieveRow; x++)
 		{	
 			for (int y=0;y<itemSieveColumn;y++)
@@ -26,7 +32,16 @@ public class Sieve {
 			}
 		}
 
-		//
+		//Sieve Method
+		for (int i=1; i<itemSieve;i++)
+		{
+			for (int j=1;j<itemSieve/i;j++)
+			{
+				primeOrNot[i][j]=false;
+				
+				
+			}
+		}
 
 	}
 
